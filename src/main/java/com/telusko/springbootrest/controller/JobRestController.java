51546147842c11c5d564a9a2a04c6df8afafd86d
dineshndr <itsmendr3@gmail.com>
2024-05-17 15:59:46 -0500

    Modified Rest Controller

---
+++ src/main/java/com/telusko/springbootrest/controller/JobRestController.java
@@ -0,0 +1,52 @@
+package com.telusko.springbootrest.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.telusko.springbootrest.model.JobPost;
+import com.telusko.springbootrest.service.JobService;
+
+import jakarta.annotation.PostConstruct;
+
+@RestController
+@CrossOrigin(origins="http://localhost:3000")
+public class JobRestController {
+	
+	@Autowired
+	private JobService service;
+	
+	
+	@GetMapping("jobPosts")
+	public List<JobPost> getAllJobs() {
+		return service.getAllJobs();
+		
+	}
+	
+	@GetMapping("/jobPost/{postId}")
+	public JobPost getJob(@PathVariable int postId) {
+		return service.getJob(postId);
+	}
+
+	@PostMapping("jobPost")
+	public JobPost addJob(@RequestBody JobPost jobPost) {
+		service.addJob(jobPost);
+		return service.getJob(jobPost.getPostId());
+	}
+	
+	@PutMapping("jobPost")
+	public JobPost updateJob(@RequestBody JobPost jobPost) {
+		service.updateJob(jobPost);
+		return service.getJob(jobPost.getPostId());
+	}
+	
+	@DeleteMapping("jobPost/{postId}")
+	public String deleteJob(@PathVariable int postId)
+	{
+		service.deleteJob(postId);
+		return "Deleted";
+	}
+	
+	
+}
